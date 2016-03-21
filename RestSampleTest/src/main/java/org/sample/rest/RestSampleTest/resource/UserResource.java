b9124b6306ffc0f70b0d20caef06c39f43b3119c
ujwal sapkota <sapkotaujwal@gmail.com>
2016-03-21 14:57:57 -0500

    changing to SpringBoot

--- RestSampleTest/src/main/java/org/sample/rest/RestSampleTest/resource/UserResource.java
+++ RestSampleTest/src/main/java/org/sample/rest/RestSampleTest/resource/UserResource.java
@@ -4,60 +4,58 @@ package org.sample.rest.RestSampleTest.resource;
 import java.util.Date;
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.sample.rest.RestSampleTest.model.UserDetails;
 import org.sample.rest.RestSampleTest.service.UserService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Path("/users")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+
+@RestController
+@Api(
+        value = "User Details",
+        description = "Methods to manage Sensitive Data Codes",
+        consumes = "application/json",
+        produces = "application/json") 
 public class UserResource {
 	
 	UserService userservice = new UserService();
 	
-	//get all users
-	@GET
-//	@Path("/getAllUsers")
-	public List<UserDetails> getAllUsersInXML() {
+    /**
+     * Get all User Details
+     * 
+     */
+    @ApiOperation(
+            httpMethod = "GET",
+            value = "Get all SensitiveDataCode records",
+            response = UserDetails.class,
+            responseContainer = "List")
+    @ApiImplicitParam(
+            name = "access_token",
+            required = false,
+            dataType = "integer",
+            paramType = "header")
+	 @RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
+	public List<UserDetails> getAllUsers() {
         return userservice.getAllUsers();
     } 
 
   
   // single user by Id
-  @GET
-  @Path("/{id}")
-  public UserDetails getUserByIdJSON(@PathParam("id")int id){
+	 @RequestMapping(value = "/getAllUsers/{id}", method = RequestMethod.GET)
+  public UserDetails getUserByIdJSON( @ApiParam(value = "Id of User Details", required = true) @PathVariable int id){
       return userservice.getUserByID(id);
   }
 	
-  
-/*  //insert new user 
-  @POST
-  @Produces(MediaType.APPLICATION_JSON)
-  @Path("/{userId}/{userName}")
-  public String saveUser(@PathParam("userId") int userId, @PathParam("userName") String userName){
-	  UserDetails userdetails = new UserDetails();
-	  userdetails.setUserID(userId);
-	  userdetails.setUserName(userName);
-	  userdetails.setDate(new Date());
-	  
-	  if (!userservice.saveUser(userdetails)){
-		  return "{\"status\":\"ok\"}"; 
-	  }
-	  else {
-		  return "{\"status\":\" not ok\"}";
-	  }
-	  
-  }*/
+  /*
   
   //insert new user test
   @POST
@@ -71,24 +69,7 @@ public class UserResource {
 	  
   } 
   
-/*  //update new user 
-  @PUT
-  @Produces(MediaType.APPLICATION_JSON)
-  @Path("/{userId}/{userName}")
-  public String updateUser(@PathParam("userId") int userId, @PathParam("userName") String userName){
-	  UserDetails userdetails = new UserDetails();
-	  userdetails.setUserID(userId);
-	  userdetails.setUserName(userName);
-	  userdetails.setDate(new Date());
-	  
-	  if (!userservice.saveUser(userdetails)){
-		  return "{\"status\":\"ok\"}"; 
-	  }
-	  else {
-		  return "{\"status\":\" not ok\"}";
-	  }
-	  
-  }*/
+
   
   
   //update new user test
@@ -131,7 +112,7 @@ public class UserResource {
         return "Got it!";
     }
 
-
+*/
 
 
 	
